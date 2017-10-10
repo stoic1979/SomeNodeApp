a190c9ebddf5f9c3fc45d553720e8570733f1727
Navjot Singh <weavebytes@gmail.com>
2017-10-10 20:56:40 +0530

    update get script

--- tests/get_students.js
+++ tests/get_students.js
@@ -6,6 +6,9 @@ let mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/studentdb';
 mongoose.connect(mongoUri, function(err){
     if(err) { console.log("Error connecting to database: ", err); return; }
 
+	//-------------------------------------------------------
+    // count no. of students
+    //-------------------------------------------------------
     Student.count(function(err, count){
         
         if(err) {
@@ -16,6 +19,27 @@ mongoose.connect(mongoUri, function(err){
        console.log("Total students: " + count);
  	})
 
-})//connect
+
+    //-------------------------------------------------------
+    // find student by name
+    //-------------------------------------------------------
+    var query = {name: 'tom'};
+
+ 	Student.findOne(query , function (err, rec) {
+
+ 		if(err) {
+       		console.log("Error getting student by name: " + query.name);
+        	return; 
+        }
+
+        if(rec) {
+        	console.log("found student: " + JSON.stringify(rec) );
+        } else {
+        	console.log("No student found by name: " + query.name);
+        }
+
+ 	});
+
+});//connect
 
 
